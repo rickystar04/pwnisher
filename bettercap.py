3cfa800dad7f080086a69c2b28f8c3ad95b0ee36
Riccardo Benincasa <riccardo@kali.Home>
2025-06-01 15:50:53 +0200

    Empty-Commit

--- bettercap.py
+++ bettercap.py
@@ -1,22 +1,22 @@
-import logging
-import requests
-import websockets
-import asyncio
+import os
+import time
+import tomlkit
 import random
 import logging
-import os
-import tomlkit
+import asyncio
+import threading
 import subprocess
+import requests
+import websockets
 
-
-from requests.auth import HTTPBasicAuth
 from time import sleep
+from requests.auth import HTTPBasicAuth
 
 logging.basicConfig(
-    level=logging.DEBUG,  # o INFO, WARNING, ERROR a seconda di cosa vuoi vedere
+    level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] %(message)s',
 )
-requests.adapters.DEFAULT_RETRIES = 5  # increase retries number
+requests.adapters.DEFAULT_RETRIES = 5
 
 ping_timeout = 180
 ping_interval = 15
@@ -38,51 +38,34 @@ def decode(r, verbose_errors=True):
             raise Exception(err)
         return r.text
 
-class Client(object):
-    def __init__(self, config, hostname='localhost', scheme='http', port=8081, username='user', password='pass', ):
+
+class Client:
+    def __init__(self, config, hostname='localhost', scheme='http', port=8081, username='user', password='pass'):
+        self.config = config
         self.hostname = hostname
         self.scheme = scheme
         self.port = port
         self.username = username
         self.password = password
-        self.url = "%s://%s:%d/api" % (scheme, hostname, port)
-        self.websocket = "ws://%s:%s@%s:%d/api" % (username, password, hostname, port)
+        self.url = f"{scheme}://{hostname}:{port}/api"
+        self.websocket = f"ws://{username}:{password}@{hostname}:{port}/api"
         self.auth = HTTPBasicAuth(username, password)
-      
-        print(config)
-        self.config=config
 
-    # session takes optional argument to pull a sub-dictionary
-    #  ex.: "session/wifi", "session/ble"
     def session(self, sess="session"):
-        r = requests.get("%s/%s" % (self.url, sess), auth=self.auth)
+        r = requests.get(f"{self.url}/{sess}", auth=self.auth)
         return decode(r)
 
     async def start_websocket(self, consumer):
-        s = "%s/events" % self.websocket
-
-        # More modern version of the approach below
-        # logging.info("Creating new websocket...")
-        # async for ws in websockets.connect(s):
-        #     try:
-        #         async for msg in ws:
-        #             try:
-        #                 await consumer(msg)
-        #             except Exception as ex:
-        #                     logging.debug("Error while parsing event (%s)", ex)
-        #     except websockets.exceptions.ConnectionClosedError:
-        #         sleep_time = max_sleep*random.random()
-        #         logging.warning('Retrying websocket connection in {} sec'.format(sleep_time))
-        #         await asyncio.sleep(sleep_time)
-        #         continue
-
-        # restarted every time the connection fails
+        s = f"{self.websocket}/events"
         while True:
             logging.info("[bettercap] creating new websocket...")
             try:
-                async with websockets.connect(s, ping_interval=ping_interval, ping_timeout=ping_timeout,
-                                              max_queue=max_queue) as ws:
-                    # listener loop
+                async with websockets.connect(
+                    s,
+                    ping_interval=ping_interval,
+                    ping_timeout=ping_timeout,
+                    max_queue=max_queue
+                ) as ws:
                     while True:
                         try:
                             async for msg in ws:
@@ -97,73 +80,62 @@ class Client(object):
                                 logging.warning('[bettercap] ping OK, keeping connection alive...')
                                 continue
                             except:
-                                sleep_time = min_sleep + max_sleep*random.random()
-                                logging.warning('[bettercap] ping error - retrying connection in {} sec'.format(sleep_time))
+                                sleep_time = min_sleep + max_sleep * random.random()
+                                logging.warning('[bettercap] ping error - retrying in %.1f sec', sleep_time)
                                 await asyncio.sleep(sleep_time)
                                 break
-            except ConnectionRefusedError:
-                sleep_time = min_sleep + max_sleep*random.random()
-                logging.warning('[bettercap] nobody seems to be listening at the bettercap endpoint...')
-                logging.warning('[bettercap] retrying connection in {} sec'.format(sleep_time))
+            except (ConnectionRefusedError, OSError):
+                sleep_time = min_sleep + max_sleep * random.random()
+                logging.warning('[bettercap] retrying websocket connection in %.1f sec', sleep_time)
                 await asyncio.sleep(sleep_time)
-                continue
-            except OSError:
-                logging.warning('connection to the bettercap endpoint failed...')
-                pwnagotchi.restart("AUTO")
 
     def run(self, command, verbose_errors=True):
         while True:
             try:
-                r = requests.post("%s/session" % self.url, auth=self.auth, json={'cmd': command})
-            except requests.exceptions.ConnectionError as e:
-                sleep_time = min_sleep + max_sleep*random.random()
-                logging.warning("[bettercap] can't run my request... connection to the bettercap endpoint failed...")
-                logging.warning('[bettercap] retrying run in {} sec'.format(sleep_time))
+                r = requests.post(f"{self.url}/session", auth=self.auth, json={'cmd': command})
+            except requests.exceptions.ConnectionError:
+                sleep_time = min_sleep + max_sleep * random.random()
+                logging.warning("[bettercap] connection error, retrying run in %.1f sec", sleep_time)
                 sleep(sleep_time)
             else:
                 break
-
         return decode(r, verbose_errors=verbose_errors)
 
     def start_module(self, module):
-        self.run('%s on' % module)
+        self.run(f'{module} on')
 
     def iface_channels(self, ifname):
         channels = []
-        phy = subprocess.getoutput("/sbin/iw %s info | grep wiphy | cut -d ' ' -f 2" % ifname)
-        output = subprocess.getoutput(r"/sbin/iw phy%s channels | grep ' MHz' | grep -v disabled | sed 's/^.*\[//g' | sed s/\].*\$//g" % phy)
+        phy = subprocess.getoutput(f"/sbin/iw {ifname} info | grep wiphy | cut -d ' ' -f 2")
+        output = subprocess.getoutput(f"/sbin/iw phy{phy} channels | grep ' MHz' | grep -v disabled | sed 's/^.*\\[//g' | sed s/\\].*\\$//g")
         for line in output.split("\n"):
-            line = line.strip()
             try:
-                channels.append(int(line))
-            except Exception as e:
+                channels.append(int(line.strip()))
+            except Exception:
                 pass
         return channels
 
-
-
     def is_module_running(self, module):
         s = self.session()
-        for m in s['modules']:
+        for m in s.get('modules', []):
             if m['name'] == module:
                 return m['running']
         return False
 
-
     def restart_module(self, module):
-        self.run('%s off; %s on' % (module, module))
-    
+        self.run(f'{module} off; {module} on')
+
     def get_config(self):
         return self.config
 
     def _reset_wifi_settings(self):
-        cfg = get_config()
+        cfg = self.get_config()
         mon_iface = cfg['main']['iface']
-        self.run('set wifi.interface %s' % mon_iface)
-        self.run('set wifi.ap.ttl %d' % cfg['personality']['ap_ttl'])
-        self.run('set wifi.sta.ttl %d' % cfg['personality']['sta_ttl'])
-        self.run('set wifi.rssi.min %d' % cfg['personality']['min_rssi'])
-        self.run('set wifi.handshakes.file %s' % cfg['bettercap']['handshakes'])
+        self.run(f'set wifi.interface {mon_iface}')
+        self.run(f'set wifi.ap.ttl {cfg["personality"]["ap_ttl"]}')
+        self.run(f'set wifi.sta.ttl {cfg["personality"]["sta_ttl"]}')
+        self.run(f'set wifi.rssi.min {cfg["personality"]["min_rssi"]}')
+        self.run(f'set wifi.handshakes.file {cfg["bettercap"]["handshakes"]}')
         self.run('set wifi.handshakes.aggregate false')
 
     def start_monitor_mode(self):
@@ -171,57 +143,49 @@ class Client(object):
         mon_start_cmd = "startmon.sh"
         has_mon = False
 
-        while has_mon is False:
+        while not has_mon:
             s = self.session()
-            for iface in s['interfaces']:
+            for iface in s.get('interfaces', []):
                 if iface['name'] == mon_iface:
                     logging.info("found monitor interface: %s", iface['name'])
                     has_mon = True
                     break
 
-            if has_mon is False:
-                if mon_start_cmd is not None and mon_start_cmd != '':
+            if not has_mon:
+                if mon_start_cmd:
                     logging.info("starting monitor interface ...")
-                    #self.run('!%s' % mon_start_cmd)
-                    subprocess.run(["bash", "startmon.sh"])                    
+                    subprocess.run(["bash", mon_start_cmd])
                 else:
                     logging.info("waiting for monitor interface %s ...", mon_iface)
                     time.sleep(1)
 
-        cfg = get_config()
-        print(cfg)
-        
+        cfg = self.get_config()
         self._supported_channels = self.iface_channels(cfg['main']['iface'])
         logging.info("supported channels: %s", self._supported_channels)
         logging.info("handshakes will be collected inside %s", cfg['bettercap']['handshakes'])
 
         self._reset_wifi_settings()
 
-        wifi_running = self.is_module_running('wifi')
-        if wifi_running:
+        if self.is_module_running('wifi'):
             logging.debug("restarting wifi module ...")
             self.restart_module('wifi.recon')
             self.run('wifi.clear')
-        elif not wifi_running:
+        else:
             logging.debug("starting wifi module ...")
             self.start_module('wifi.recon')
 
-
     def start_event_polling(self):
-        # start a thread and pass in the mainloop
-        #_thread.start_new_thread(self._event_poller, (asyncio.get_event_loop(),))
-        threading.Thread(target=self._event_poller, args=(asyncio.get_event_loop(),), name="Event Polling", daemon=True).start()
+        threading.Thread(
+            target=self._event_poller,
+            args=(asyncio.get_event_loop(),),
+            name="Event Polling",
+            daemon=True
+        ).start()
 
     def recon(self):
-        cfg = get_config()
-        print(cfg)
+        cfg = self.get_config()
         recon_time = cfg['personality']['recon_time']
-        max_inactive = cfg['personality']['max_inactive_scale']
-        recon_mul = cfg['personality']['recon_inactive_multiplier']
         channels = cfg['personality']['channels']
-
- 
-
 
         if not channels:
             self._current_channel = 0
@@ -230,83 +194,39 @@ class Client(object):
         else:
             logging.debug("RECON %ds ON CHANNELS %s", recon_time, ','.join(map(str, channels)))
             try:
-                self.run('wifi.recon.channel %s' % ','.join(map(str, channels)))
+                self.run(f'wifi.recon.channel {",".join(map(str, channels))}')
             except Exception as e:
-                logging.exception("Error while setting wifi.recon.channels (%s)", e)
+                logging.exception("Error setting wifi.recon.channels: %s", e)
 
-        
 
 def load_toml_file(filename):
-        """Load toml data from a file. Use toml for dotted, tomlkit for nice formatted"""
-        with open(filename) as fp:
-            text = fp.read()
-        # look for "[main]". if not there, then load
-        # dotted toml with toml instead of tomlkit
-        if text.find("[main]") != -1:
-            return tomlkit.loads(text)
-        else:
-            print("Converting dotted toml %s: %s" % (filename, text[0:100]))
-            import toml
-            data = toml.loads(text)
-            # save original as a backup
-            try:
-                backup = filename + ".ORIG"
-                os.rename(filename, backup)
-                with open(filename, "w") as fp2:
-                    tomlkit.dump(data, fp2)
-                print("Converted to new format. Original saved at %s" % backup)
-            except Exception as e:
-                print("Unable to convert %s to new format: %s" % (backup, e))
-            return data
-    
-        return config
-
-    # load the defaults
-  
+    with open(filename) as fp:
+        text = fp.read()
+    if "[main]" in text:
+        return tomlkit.loads(text)
+    else:
+        import toml
+        print("Converting dotted toml:", filename)
+        data = toml.loads(text)
+        try:
+            backup = filename + ".ORIG"
+            os.rename(filename, backup)
+            with open(filename, "w") as fp2:
+                tomlkit.dump(data, fp2)
+            print("Converted to new format. Original saved at", backup)
+        except Exception as e:
+            print("Unable to convert to new format:", e)
+        return data
 
 
 def start(args):
-
-    import time
-
-
     config = load_toml_file(args.config)
-    print(config)
+    logging.info("Loaded config: %s", config)
 
-    
+    hostname = config['bettercap'].get('hostname', '127.0.0.1')
+    client = Client(
+        config,
+        hostname=hostname
+    )
 
-    print("Client è di tipo:", type(Client))
-   # client = Client(config)
-    client= Client( config,
-                        "127.0.0.1" if "hostname" not in config['bettercap'] else config['bettercap']['hostname'],
-                        "http" if "scheme" not in config['bettercap'] else config['bettercap']['scheme'],
-                        8081 if "port" not in config['bettercap'] else config['bettercap']['port'],
-                        "pwnagotchi" if "username" not in config['bettercap'] else config['bettercap']['username'],
-                        "pwnagotchi" if "password" not in config['bettercap'] else config['bettercap']['password'])
-
-    
-    
-    client.session()
-
-    client.start_monitor_mode()
-
-    client.recon()
-
-    while True:
-        session = client.session("session/wifi")
-
-        # Lista di Access Point
-        for ap in session.get('aps', []):
-            ssid = ap.get('ssid', '<hidden>')
-            bssid = ap.get('bssid')
-            rssi = ap.get('rssi')
-            clients = ap.get('clients', [])
-            print(f"[AP] SSID: {ssid}, BSSID: {bssid}, RSSI: {rssi}, Clients: {len(clients)}")
-
-            # Lista di client associati
-            for client in clients:
-                mac = client.get('mac')
-                rssi = client.get('rssi')
-                print(f"    [Client] MAC: {mac}, RSSI: {rssi}")
-
-        time.sleep(5)
+    logging.info("Client è di tipo: %s", type(client))
